62dbe4dfb56bf661c01028c4517c6ab8089bfcfd
Chad Smith <cds021@yahoo.com>
2023-11-14 15:25:12 -0500

    updating client files

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,33 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
+  // Database version
+  const jateDB = await openDB('jate', 1);
+  // New transaction
+  const tx = jateDB.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  //Pass in content
+  const request = store.put({ id: 1, value: content });
+  // Confirmation
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+export const getDb = async () => {
+  console.error('getDb not implemented');
+    // Database version
+    const jateDB = await openDB('jate', 1);
+    // New transaction
+    const tx = jateDB.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    //Pass in content
+    const request = store.getAll();
+    // Confirmation
+    const result = await request;
+    console.log('🚀 - data saved to the database', result);
+}
 initdb();
